231a2eddab1c71d5a3446c50240af96cf9330836
zabelmarta <104087600+zabelmarta@users.noreply.github.com>
2023-01-31 19:56:13 +0100

    zad 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const gallery = document.querySelector('.gallery');
+const createGallery = createCardImg(galleryItems);
+gallery.insertAdjacentHTML('beforeend', createGallery);
+
+
+function createCardImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"></a>`;
+    })
+    .join("");
+}
+
+const ligthBox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  close: false,
+});
