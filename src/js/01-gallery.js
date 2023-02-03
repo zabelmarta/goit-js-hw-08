ac99b8a1b164c0dad1544ea08ea3399cd8283060
zabelmarta <104087600+zabelmarta@users.noreply.github.com>
2023-02-03 16:14:03 +0100

    nada nie działa

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,24 +5,38 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-const gallery = document.querySelector('.gallery');
-const createGallery = createCardImg(galleryItems);
-gallery.insertAdjacentHTML('beforeend', createGallery);
+// const gallery = document.querySelector('.gallery');
+// const createGallery = createCardsImg(galleryItems);
 
 
-function createCardImg(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-    <a class="gallery__item" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}"></a>`;
-    })
-    .join("");
-}
+// function createCardsImg(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+     
+//     `<a class="gallery__item" href="${original}">
+//     <img class="gallery__image" src="${preview}" alt="${description}"></a>`;
+//     })
+//     .join("");
+// }
 
-const ligthBox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250,
-  close: false,
+// const ligthBox = new SimpleLightbox(".gallery__item", {
+//   captionsData: "alt",
+//   captionDelay: 250,
+//   close: false,
+// });
+
+
+// gallery.insertAdjacentHTML('beforeend', createGallery);
+
+const galleryElement = document.querySelector('.gallery');
+
+let galleryTemplate = '';
+galleryItems.forEach(item => {
+  galleryTemplate += `
+    <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+    </a>
+  `;
 });
+
+galleryElement.insertAdjacentHTML('beforeend', galleryTemplate);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,64 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+onReload();
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
-    localStorage.setItem('videoplayer-current-time(|)', seconds);
+function onPlay(timeupdate) {
+  const videoCurrentTime = timeupdate.seconds.toString();
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, videoCurrentTime);
+}
+
+function onReload() {
+  const savedLocalStorageTime = Number(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if (savedLocalStorageTime) {
+    console.log(savedLocalStorageTime);
+    player
+      .setCurrentTime(savedLocalStorageTime)
+      .then(function () {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+
+          default:
+            break;
+        }
+      });
   }
-  
-player.setCurrentTime(localStorage.getItem ('videoplayer-current-time(|)')||0);
+}
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+
+
+// const savedTime = localStorage.getItem("videoplayer-current-time");
+
+
+// player.setCurrentTime(savedTime).then(function(seconds) {
+
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+
+//             break;
+
+//         default:
+
+//             break;
+//     }
+// });
+
+// player.on('timeupdate', throttle(function (timeObj) {
+//     localStorage.setItem("videoplayer-current-time", JSON.stringify(timeObj.seconds)); 
+// }, 1000));
+
+
+
